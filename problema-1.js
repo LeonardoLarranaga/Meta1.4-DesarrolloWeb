4f96ec6a2f0fc850877fb666a8b15b4c24e0c9f7
Leonardo Larrañaga <luis.larranaga@UABC.edu.mx>
2024-02-19 11:50:21 -0800

    Implementación try/catch en el problema 1.

--- problema-1.js
+++ problema-1.js
@@ -2,54 +2,62 @@ const claves = [12395, 57239, 16568, 17532, 30928, 57239, 94124, 18471, 48303, 4
 
 const nombres = ['Alejandra Ramirez', 'Javier Lopez', 'Valentina Hernandez', 'Diego Garcia', 'Camila Martinez', 'Santiago Rodriguez', 'Isabella Gomez', 'Mateo Torres', 'Renata Flores', 'Emiliano Vargas']
 
-let elementoRandom = function(lista) {
+let elementoRandom = function (lista) {
     return lista[Math.floor(Math.random() * lista.length)]
 }
 
-let calificacionRandom = function() {
+let calificacionRandom = function () {
     return Math.round(Math.random() * 10000) / 100;
 }
 
-let fechaRandom = function() {
+let fechaRandom = function () {
     const dateInicial = new Date("2021-01-01")
     const dateFinal = new Date("2024-06-30")
 
     return new Date(dateInicial.getTime() + Math.random() * (dateFinal.getTime() - dateInicial.getTime()))
 }
 
-let creditoRandom = function() {
+let creditoRandom = function () {
     return Math.floor(Math.random() * 5) + 4
 }
 
 const registros = []
 
-N = process.argv[2]
+try {
+    const N = process.argv[2]
 
-for (let i = 0; i < N; i++) {
-    registros.push({
-        clave: elementoRandom(claves),
-        creditos: creditoRandom(),
-        nombre: elementoRandom(nombres),
-        calificacion: calificacionRandom(),
-        fecha: fechaRandom()
-    })
+    if (N == undefined || N === null) {
+        throw new Error("No se encontró un argumento en la línea de comandos.")
+    }
+
+    for (let i = 0; i < N; i++) {
+        registros.push({
+            clave: elementoRandom(claves),
+            creditos: creditoRandom(),
+            nombre: elementoRandom(nombres),
+            calificacion: calificacionRandom(),
+            fecha: fechaRandom()
+        })
+    }
+
+    let filtarRegistros = function (registros) {
+        // Fecha actual menos seis meses:
+        const fechaMenor = new Date()
+        fechaMenor.setMonth(fechaMenor.getMonth() - 6)
+
+        return registros.filter(r => r.calificacion < 60 && r.fecha > fechaMenor)
+    }
+
+    let mapearRegistros = function (registros) {
+        return registros.map(r => ({
+            clave: r.clave,
+            calificacion: r.calificacion,
+            fechaStr: `${r.fecha.getDate().toString().padStart(2, '0')}/${(r.fecha.getMonth() + 1).toString().padStart(2, '0')}/${r.fecha.getFullYear()}`
+        }))
+    }
+
+    console.log("Registros:\n", registros)
+    console.log("\nRegistros con calificación menor a 60 y fecha mayor a fecha actual menos 6 meses:\n", mapearRegistros(filtarRegistros(registros)))
+} catch (error) {
+    console.error("Error: ", error.message)
 }
-
-let filtarRegistros = function(registros) {
-    // Fecha actual menos seis meses:
-    const fechaMenor = new Date()
-    fechaMenor.setMonth(fechaMenor.getMonth() - 6)
-
-    return registros.filter(r => r.calificacion < 60 && r.fecha > fechaMenor)
-}
-
-let mapearRegistros = function(registros) {
-    return registros.map(r => ({
-        clave: r.clave,
-        calificacion: r.calificacion,
-        fechaStr: `${r.fecha.getDate().toString().padStart(2, '0')}/${(r.fecha.getMonth() + 1).toString().padStart(2, '0')}/${r.fecha.getFullYear()}`
-    }))
-}
-
-console.log("Registros:\n", registros)
-console.log("\nRegistros con calificación menor a 60 y fecha mayor a fecha actual menos 6 meses:\n", mapearRegistros(filtarRegistros(registros)))
